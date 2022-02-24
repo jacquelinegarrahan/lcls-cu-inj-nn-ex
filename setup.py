a43316d93057468c46d286265da0afd5eaa9e8e3
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-02-24 14:33:40 -0800

    Check in initial package setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os import path, environ
+#import versioneer
+
+cur_dir = path.abspath(path.dirname(__file__))
+
+# parse requirements
+with open(path.join(cur_dir, "requirements.txt"), "r") as f:
+    requirements = f.read().split()
+
+
+setup(
+    name="lcls_cu_inj_nn_ex",
+    version=0.0,
+   # cmdclass=versioneer.get_cmdclass(),
+    author="SLAC National Accelerator Laboratory",
+    author_email="jgarra@slac.stanford.edu",
+    license="SLAC Open",
+    packages=find_packages(),
+    install_requires=requirements,
+    # set up development requirements
+    extras_require={
+        "test": ["pytest"],
+    },
+    url="https://github.com/jacquelinegarrahan/lcls-cu-inj-nn-ex",
+    include_package_data=True,
+    python_requires=">=3.7",
+)
