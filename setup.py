53174631bbd17108b918ebeece9d4f382d932757
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-02-24 14:40:30 -0800

    Check in versioning

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 from os import path, environ
-#import versioneer
+import versioneer
 
 cur_dir = path.abspath(path.dirname(__file__))
 
@@ -12,16 +12,12 @@ with open(path.join(cur_dir, "requirements.txt"), "r") as f:
 setup(
     name="lcls_cu_inj_nn_ex",
     version=0.0,
-   # cmdclass=versioneer.get_cmdclass(),
+    cmdclass=versioneer.get_cmdclass(),
     author="SLAC National Accelerator Laboratory",
     author_email="jgarra@slac.stanford.edu",
     license="SLAC Open",
     packages=find_packages(),
     install_requires=requirements,
-    # set up development requirements
-    extras_require={
-        "test": ["pytest"],
-    },
     url="https://github.com/jacquelinegarrahan/lcls-cu-inj-nn-ex",
     include_package_data=True,
     python_requires=">=3.7",
