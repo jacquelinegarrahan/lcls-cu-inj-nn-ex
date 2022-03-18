ee22710aadf4a64c775d72eb8baf474c65fdbd33
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-03-18 10:01:54 -0700

    Add scaling handling to flow

--- lcls_cu_inj_nn_ex/__init__.py
+++ lcls_cu_inj_nn_ex/__init__.py
@@ -1,5 +1,7 @@
 from . import _version
 from pkg_resources import resource_filename
+from lume_model.utils import variables_from_yaml
+
 __version__ = _version.get_versions()['version']
 
 
@@ -14,3 +16,17 @@ VARIABLE_FILE =resource_filename(
 EPICS_CONFIG_FILE =resource_filename(
     "lcls_cu_inj_nn_ex.files", "epics_config.yml"
 )
+
+
+CU_INJ_MAPPING = resource_filename(
+    "lcls_cu_inj_nn_ex.files", "cu_inj_impact.csv"
+)
+
+
+
+with open(VARIABLE_FILE, "r") as f:
+    INPUT_VARIABLES, OUTPUT_VARIABLES = variables_from_yaml(f)
+
+
+CU_INJ_MAPPING_TABLE= pd.read_csv(CU_INJ_MAPPING)
+CU_INJ_MAPPING_TABLE.set_index("impact_name")
